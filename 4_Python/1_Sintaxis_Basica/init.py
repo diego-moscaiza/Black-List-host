47ef4cae43eb05da33c7204e871a90be52b010ad
Diego Moscaiza Anampa <51138014+DMOSCAIZA@users.noreply.github.com>
2023-06-20 11:49:57 -0500

    Update python

---
+++ 4_Python/1_Sintaxis_Basica/init.py
@@ -0,0 +1,107 @@
+# SINTAXIS DE PYTHON
+
+
+# IMPRIMIR MENSAJES
+print('Hola Mundo')
+
+# VARIBALES
+
+#En cadena o string
+libro = 'NUEVO LIBRO'
+
+#Numericos
+entero = 100
+decimal = 3.14165467
+
+#Booleanos (Si o No)
+autorizado = True
+seleccionado = False
+
+# LISTAS
+numeros = [23, 34, 675, 783]
+animales = ['perro,' 'gato', 'ornitorrinco']
+datosMixtos = ['texto', 40, True, ['lista interna']]
+
+#Accediendo a un valor de la lista colocando su indice o posición.
+numeros[1]
+#print(numeros[1])
+
+# DICCIONARIO. Trabaja con "clave : valor"
+
+jugadores = {
+    10: 'Messi',
+    7: 'Ronaldo'
+}
+
+print(jugadores[7]) #Imprimirá el valor correspindiente a su clave
+
+paises = {
+    'EC': 'Ecuador',
+    'MX': 'México',
+    'AR': 'Argentina'
+}
+
+emails = {
+    'Juan': ['juan@gmail.com'],
+    'Ricardo': ['ricardo@gmail.com', 'ricardo@hotmail.com']
+}
+
+# CONSTANTES. Valores que no cambiaran su valor nunca.
+PI = 3.1416
+
+# OPERADORES. Simboles aritméticos
+print(1 + 1) # sumar
+print(3 - 1) # restar
+print(20 * 4) # multiplicar
+print(25 / 5) # dividir
+
+#Operadores comparativos
+print(4 == 4) # Comparar si son iguales, tanto en valor como tipo de dato 
+print(4 == '4') # 
+print(4 != 5) # Comparar si NO son iguales, tanto en valor como tipo de dato
+print(4 < 5) # Comparar si es menor.
+print(4 >= 5) # Comparar si es mayor igual.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
