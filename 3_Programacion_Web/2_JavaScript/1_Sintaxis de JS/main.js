42fbb866161d73567cf2e8df495d26ba2c7c3799
Diego Moscaiza Anampa <51138014+DMOSCAIZA@users.noreply.github.com>
2023-06-21 10:56:59 -0500

    Update JS

---
+++ 3_Programacion_Web/2_JavaScript/1_Sintaxis de JS/main.js
@@ -0,0 +1,70 @@
+// TIPOS DE DATOS (primitivos)
+
+let string = 'TEXTO'
+let number = 1232
+let bool_true = true
+let bool_false = false
+let sinDefinir = undefined
+let nulo = null
+
+// Renderización de datos
+
+document.write('texto')
+
+alert(true)
+
+// TIPOS DE DATOS (No primitivos)
+
+let lista = [1, 2, 3, 4, 5, 6]
+
+let objetoDiccionario = { LIM: 'Lima'} // es un objeto con formato de clave : valor
+
+
+// VARIABLES
+
+let telefono = '948752900'
+alert(telefono)
+
+// OPERADORES ARITMÉTICOS
+
+let sumar  = 1 + 1
+let restar = 9 - 3
+let dividir = 20 + 4
+let multiplicar = 2 * 5
+
+// OPERADORES comparativos
+
+4 == 4 // Devolvertá TRUE
+0 == '0' // Devolverá TRUE porque compara solo el valor y no el tipo de dato
+0 === '0' // Devolverá FALSE ya que se compara con 'triple =' el valor y tipo de dato
+
+// OPERADORES logicos
+
+// Se usa el operador 'and' colocando 'doble &'
+
+true && true  // VERDADERO
+true && false // FALSO
+false && true // FALSO
+false && false // FALSO
+
+// Se usa el operador 'and' colocando 'doble ||'
+
+true && true  // VERDADERO
+true && false // FALSO
+false && true // FALSO
+false && false // FALSO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
