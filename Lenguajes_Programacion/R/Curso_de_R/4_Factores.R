4ff67b8ffcf8c5a83848ccbad1df5878a8b15f25
Diego Moscaiza <diego-moscaiza@outlook.com>
2023-11-07 21:15:13 -0500

    Update R Course

--- Lenguajes_Programacion/R/Curso_de_R/4_Factores.R
+++ Lenguajes_Programacion/R/Curso_de_R/4_Factores.R
@@ -1,4 +1,10 @@
-# video 07 – factores
+# Factores
+
+# - Es una estructura de datos para manejar variables categóricas.
+#   Son datos que toman un cantidad finita de valores como:
+#   "rangos de edades", "dias de la semana", "colores", etc.
+# - Las variables categóricas, en estadística, 
+#   se tratan difenrente a las variables continuas o numéricas.
 
 # Objetivo: estudiar qué son factores en R.
 # ——————————————–
@@ -12,55 +18,68 @@
 # correr esto antes de empezar…   #
 #####################################
 
-# crear vector de ventas
+# 1. crear vector de ventas
 tallas <- c('m', 'g', 'S', 'S','m', 'M')
 
-# intentar graficar
-# plot(tallas).
+# 2. intentar graficar
+# plot(tallas). 
+
+# - Muestra error al momento de graficas porque necesitamos crear un factor
 
 ###################################
 # práctica 1: crear factores en R #
 ###################################
 
-# crear factor de un vector
-
+# 1. crear factor de un vector
+# - Para usar la función "factor()" se necesita un vector para añadirlo como argumento o parámetro.
 tallas_factor <- factor(tallas)
 
-# graficar factor
-
+# 2. graficar factor
 plot(tallas_factor)
 
-# mirar niveles de factor
-
+# 3. mirar niveles de factor
+# - Las categorias o clases de un factor se llaman niveles y con la función "levels()"
 levels(tallas_factor)
 
 ######################################
 # práctica 2: recodificando factores #
 ######################################
 
-# creando factor recodificado
+# 1. creando factor recodificado
+
+# Estructura de un factor recodificado:
+# factor(vector, 
+#       levels = vector_niveles, # Etiqueta de niveles existentes
+#       labels = vector_etiqueta) # Nueva etiqueta de niveles
 
 tallas_recodificado <- factor(tallas,
                             levels = c("g", "m", "M", "S"),
                             labels = c("G", "M", "M", "S"))
 
-# graficando ventas_recodificado
+# 2. graficando ventas_recodificado
 plot(tallas_recodificado)
 
 #############################################
 # práctica 3: ordenando niveles de factores #
 #############################################
 
-# ordenando niveles (copiar factor anterior)
+# 1. ordenando niveles (copiar factor anterior)
+
+# Estructura de un factor recodificado ordenado:
+# factor(vector,
+#       ordered = TRUE # Definimos con TRUE para confirmar el ordenamiento.
+#       levels = vector_niveles, # Etiqueta de niveles existentes
+#       labels = vector_etiqueta) # Nueva etiqueta de niveles
 
 tallas_ordenado <- factor(tallas,
                             ordered = TRUE,
                             levels = c("S", "m", "M", "g"),
                             labels = c("S", "M", "M", "G"))
 
-# viendo el orden en los niveles
+# 2. viendo el orden en los niveles
 
 tallas_ordenado
 
-# graficando el factor ordenado
+# 3. graficando el factor ordenado
 plot(tallas_ordenado)
+
