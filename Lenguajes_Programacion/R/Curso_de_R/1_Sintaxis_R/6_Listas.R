629729d969937b0c141305a5fc15f5700b95e841
Diego Moscaiza <diego-moscaiza@outlook.com>
2023-11-12 22:47:58 -0500

    Sorting files and update content in R Course

--- Lenguajes_Programacion/R/Curso_de_R/1_Sintaxis_R/6_Listas.R
+++ Lenguajes_Programacion/R/Curso_de_R/1_Sintaxis_R/6_Listas.R
@@ -1,6 +1,7 @@
 # Listas
 
-# - .
+# - En R, las listas pueden incluir todas las demás estructuras de datos, como vectores, matrices, dataframes y otros que existen en este lenguaje.
+# - También sirven para guardar los resultados por ejemplo los resultados de una regresión lineal que muestra una lista que contiene: 'coeficientes estimados', 'vectores con residuos' y etc.
 
 # Objetivo: estudiar qué son las listas en R
 # ——————————————-
@@ -38,15 +39,20 @@ peliculas_df <- data.frame(nombre,
 # práctica 1: crear una lista en R #
 ####################################
 
-# 1. crear lista en R
+# 1. crear lista en R, usando la palabra reservada 'list'
+# - Estructura Lista:
+# list(objeto1,
+#      objeto2,
+#     ...)
 
 lista_curso <- list(vector_titulos, matriz_peliculas)
 
-# 2. mostrar lista
+# 2. mostrar lista colocando el nombre la variable
 
 lista_curso
+# O podemos usar la función 'View()' y mostrará en una ventana aparte.
 
-# 3. cambiar nombre de dataframe
+# 3. cambiar nombre de la lista con la función 'name()'
 
 names(lista_curso) <- c("vector", "matriz")
 
@@ -57,11 +63,14 @@ lista_curso
 # práctica 2: Seleccionar elementos de una lista #
 ##################################################
 
-# 1. Seleccionar vector de la lista
+# 1. Seleccionar vector de la lista 
+# con doble corchete y su nombre así, '[[nombre]]' 
 lista_curso[["vector"]]
 
 # 2. Seleccionar el tercer elemento del vector de la lista
-lista_curso[["vector"]][3]
+# con un corchete al costado de la variable de la lista
+# y colocando la posición que deseamos, de esta forma, '[['nombre_objeto']]['numero_posicion']' 
+lista_curso[["vector"]][2]
 
 # 3. Seleccionar fila 5 y columna 3 de la matriz de la lista
 lista_curso[["matriz"]][5, 3]
@@ -72,12 +81,14 @@ lista_curso[["matriz"]][5, 3]
 ######################################################
 
 # 1. agregar dataframe a lista
+# Así, lista[['nombre_objeto']] <- 'objeto'
 lista_curso[["data_frame"]] <- peliculas_df
 
 # 2. revisar que está el dataframe
 lista_curso
 
 # 3. eliminar un elemento de lista
+# Asignando 'NULL' al valor así, lista_curso[["nombre_objeto"]] <- NULL
 lista_curso[["vector"]] <- NULL
 
 # 4. revisar que no está el vector
