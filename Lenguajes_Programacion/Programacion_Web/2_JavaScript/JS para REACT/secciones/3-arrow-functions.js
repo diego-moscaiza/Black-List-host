809c8793b4d41ca3a3b01894b802568769e26862
Diego Moscaiza <diego-moscaiza@outlook.com>
2023-11-12 21:14:06 -0500

    Update content in JS from React

--- Lenguajes_Programacion/Programacion_Web/2_JavaScript/JS para REACT/secciones/3-arrow-functions.js
+++ Lenguajes_Programacion/Programacion_Web/2_JavaScript/JS para REACT/secciones/3-arrow-functions.js
@@ -1,19 +1,24 @@
 /* ---------- 3.-  ARROW FUNCTION -> LAS FUNCIONES FLECHAS ---------- */
 
-// Declarando función:
+// Declarando una función normal:
 function nameFunction() {
     return "Hola"
 }
 
-// Funcion Flecha '=>':
+// Declarando una funcion flecha '=>':
 
-// Se crea una constante luego se asigna con un paréntesis '()' para los parametros de la función
-// Después colocamos una flecha '=>' y por último se escribe lo que se desea ejecutar.
+/* ESTRUCTURA:
+- Se crea una constante luego se asigna con un paréntesis '()' para los parametros de la función, después colocamos una flecha '=>' y por último se escribe lo que se desea ejecutar.*/
+
+// - Aquí los corchetes y el 'return' son implicitos.
 const newArrowFunction1 = () => "I'm an arrowFunction"
 
-// Si fueran más lineas después de la flecha abrimos corchetes.
+// - Pero si fueran más líneas podemos usar los corchetes y el 'return'.
+// - Y si añadimos una variable o mas adicional dentro de la función y queremos un resultado con esa o esas variables adicionales se tiene que retornar la función usando el 'return' con ese valor.
 const newArrowFunction2 = () => {
-    "Yes, I'm an arrowFunction, again ツ"
+    let message = "Yes, I'm an arrowFunction, again ツ"
+    return message
+    // return "Yes, I'm an arrowFunction, again ツ" // Devuelve lo mismo
 }
 
 newArrowFunction1(); // Ejecutar funcion
@@ -22,26 +27,26 @@ newArrowFunction1(); // Ejecutar funcion
 // Otro ejemplo:
 const array1 = [1, 2, 3, 4];
 
-// 0 + 1 + 2 + 3 + 4
+// '10' + 1 + 2 + 3 + 4
 const initialValue = 10;
 
 const sumWithInitial = array1.reduce(
-    // Estrctura usando una "funcion regular"
+    // - Estrctura usando una "funcion regular"
     function (accumulator, currentValue) {
         return accumulator + currentValue
     }, initialValue
 );
 
 const sumWithInitial1 = array1.reduce(
-    // Estrctura usando una "funcion flecha"
+    // - Estrctura usando una "funcion flecha"
     (accumulator, currentValue) => accumulator + currentValue,
     initialValue
 );
-// En este caso se puede añadir un valor fuera de la función interna y cona la función flecha esta puede estar incluida.
+// - En este caso se puede añadir un valor fuera de la función interna y cona la función flecha esta puede estar incluida.
 
 console.log(sumWithInitial1);
+// Retornará 20 en ambas.
 
-// - Nota: 1. Los corchetes y el 'return' son implicitos si usamos la o las variables definidas en los parámetros pero si añadimos una variable adicional dentro de la función y queremos un resultado con esa o esas variables adicionales se tiene que retornar la función usando el 'return' con ese valor.
 
 
 // FUNCTION WHIT JSX (USED ON REACT)
@@ -53,4 +58,4 @@ function ListaDeTareas(){
     );
 }
 
-// Para más sobre funciones flechas: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+// - Para más sobre funciones flechas: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Functions/Arrow_functions
