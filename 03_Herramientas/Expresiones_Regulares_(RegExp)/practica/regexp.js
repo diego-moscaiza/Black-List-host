413893b39b3a6aeb1aefbacaadace70c0304b3db
Diego Moscaiza <diego-moscaiza@outlook.com>
2023-11-28 18:53:06 -0500

    Sorting content in RegExp Js

--- 03_Herramientas/Expresiones_Regulares_(RegExp)/practica/regexp.js
+++ 03_Herramientas/Expresiones_Regulares_(RegExp)/practica/regexp.js
@@ -4,38 +4,40 @@
 const regExpNew = new RegExp("ab+c");
 const regExp = /ab+c/;
 
-// Métodos de las expresiones regulares
-// - exec(). Ejecuta una búsqueda por una coincidencia en un texto. En consola muestra un arreglo con la informacion de la ejecución.
-// - test(). Prueba una coincidencia en una cadena. Devuelve true o false.
-// - match(). Devuelve un arreglo que contiene todas las coincidencias, incluidos los grupos de captura, o null si no se encuentra ninguna coincidencia.
-// - matchAll(). Devuelve un iterador que contiene todas las coincidencias, incluidos los grupos de captura.
-// - search(). Prueba una coincidencia en una cadena. Devuelve el índice de la coincidencia, o -1 si la búsqueda falla.
-// - replace(). Ejecuta una búsqueda por una coincidencia en una cadena y reemplaza la subcadena coincidente con una subcadena de reemplazo.
-// - replaceAll(). Ejecuta una búsqueda de todas las coincidencias en una cadena y reemplaza las subcadenas coincidentes con una subcadena de reemplazo.
-// - split(). Utiliza una expresión regular o una cadena fija para dividir una cadena en un arreglo de subcadenas.
-
+/*
+// - Métodos de las expresiones regulares
+// 1. exec(). Ejecuta una búsqueda por una coincidencia en un texto. En consola muestra un arreglo con la informacion de la ejecución.
+// 2. test(). Prueba una coincidencia en una cadena. Devuelve true o false.
+// 3. match(). Devuelve un arreglo que contiene todas las coincidencias, incluidos los grupos de captura, o null si no se encuentra ninguna coincidencia.
+// 4. matchAll(). Devuelve un iterador que contiene todas las coincidencias, incluidos los grupos de captura.
+// 5. search(). Prueba una coincidencia en una cadena. Devuelve el índice de la coincidencia, o -1 si la búsqueda falla.
+// 6. replace(). Ejecuta una búsqueda por una coincidencia en una cadena y reemplaza la subcadena coincidente con una subcadena de reemplazo.
+// 7. replaceAll(). Ejecuta una búsqueda de todas las coincidencias en una cadena y reemplaza las subcadenas coincidentes con una subcadena de reemplazo.
+// 8. split(). Utiliza una expresión regular o una cadena fija para dividir una cadena en un arreglo de subcadenas.
+*/
 
 // 1. La expresión regular que sólo contiene texto, números o símbolos sólo se podrá encontrar la conincidencia una sola vez
 const regExp1 = /sol/;
 
 // - Ejemplo 1: Usando 'exec()'
 
-// - Forma breve con 'exec()'
+// 1.1 Forma breve con 'exec()'
 let frase1 = regExp1.exec("hoy parece que va a salir el sol")
 console.log(frase1);
 
-// - Forma amplia con 'exec()'
+// 1.2 Forma amplia con 'exec()'
 const frase1_1 = "hoy parece que va a salir el sol";
 const validacion1 = regExp1.exec(frase1_1);
 console.log(validacion1); // Output -> index: 29. Index se refiere a la posicion del texto donde se encontró la coincidecnia.
 
-// - Ejemplo 2: Usando 'match()' no se podrá encontrar más de una coincidencia porque la expresion regular no está definida con el modificador global 'g'
+// - Ejemplo 2:
+// Usando 'match()' no se podrá encontrar más de una coincidencia porque la expresion regular no está definida con el modificador global 'g'
 
-// - Forma breve con 'match()'
+// 2.1 Forma breve con 'match()'
 const otraFrase = ("solamente quiero comer solo").match(regExp1);
 console.log(otraFrase);
 
-// - Forma amplia con 'match()'
+// 2.2 Forma amplia con 'match()'
 const otraFrase1 = "solamente quiero comer solo";
 const otraValidacion1 = otraFrase1.match(regExp1);
 console.log(otraValidacion1);
@@ -126,42 +128,40 @@ const frase14 = "05523445523188055";
 const validacion14 = frase14.match(regExp14);
 console.log(validacion14);
 
-// 14. El operador '?' sirve para que se cumpla o no una coincidencia una sola vez. En otras palabras, es opcional.
+// 15. El operador '?' sirve para que se cumpla o no una coincidencia una sola vez. En otras palabras, es opcional.
 const regExp15 = /abz?/g;
 const frase15 = "abacccabzabzzz";
 const validacion15 = frase15.match(regExp15);
 console.log(validacion15);
 
-// 15. Con las llaves '{}' solo permite que se encuentre la coincidencia la cantidad de veces que se defina adentro.
+// 16. Con las llaves '{}' solo permite que se encuentre la coincidencia la cantidad de veces que se defina adentro.
 
 // - Ejemplo 1: es necesario los parentesis para un texto
-
 const regExp16 = /(lol){2}/g;
 const frase16 = "lollollol";
 const validacion16 = frase16.match(regExp16);
 console.log(validacion16);
 
 // - Ejemplo 2:
-
 const regExp16_1 = /5{3}/g;
 const frase16_1 = "2233555556";
 const validacion16_1 = frase16_1.match(regExp16_1);
 console.log(validacion16_1);
 
-// 16. Con el símbolo '^' estamos buscando coincidencias al inicio de una cadena de texto.
+
+// 17. Con el símbolo '^' estamos buscando coincidencias al inicio de una cadena de texto.
 const regExp17 = /^(www)/;
 const frase17 = "www.mycompany.com";
 const validacion17 = frase17.match(regExp17);
 console.log(validacion17);
 
-// 17. Con el símbolo '$' estamos buscando coincidencias al final de una cadena de texto.
+// 18. Con el símbolo '$' estamos buscando coincidencias al final de una cadena de texto.
 const regExp18 = /(com)$/;
 const frase18 = "mycompany.com";
 const validacion18 = frase18.match(regExp18);
 console.log(validacion18);
 
-// 18. Con el cuantificador '?=' analiza la coincidencia que esté antes de lo que se desea buscar.
-
+// 19. Con el cuantificador '?=' analiza la coincidencia que esté antes de lo que se desea buscar.
 const regExp19 = /(?=\.\A)/;
 const frase19 = "file.txt";
 const validacion19 = frase19.match(regExp19);
