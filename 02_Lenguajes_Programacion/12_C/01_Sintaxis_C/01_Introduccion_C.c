6eec25bd35a1fcb8b233c3e330446f1e16e80638
Diego Moscaiza <diego-moscaiza@outlook.com>
2023-12-25 22:50:58 -0500

    Adding new language

---
+++ 02_Lenguajes_Programacion/12_C/01_Sintaxis_C/01_Introduccion_C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10; // integer
+    float b = 12.3; // float
+    double c = 12.3456789; // double
+    char d = 'A'; // character
+    char str[] = "Hello World!"; // string
+
+    printf("Integer: %d\n", a);
+    printf("Float: %f\n", b);
+    printf("Double: %lf\n", c);
+    printf("Character: %c\n", d);
+    printf("String: %s\n", str);
+
+    return 0;
+}
